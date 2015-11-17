46ae78d6c5a1a41897587baf3623fca077aaca8a
Renato Soares <renatosoarespro@hotmail.com>
2015-11-17 16:32:56 -0300

    Autenticação (login e logout)

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,6 +1,7 @@
 <?php
 // app/Model/User.php
 App::uses('AppModel', 'Model');
+App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
 
 class User extends AppModel {
     public $validate = array(
@@ -24,4 +25,15 @@ class User extends AppModel {
             )
         )
     );
+
+    
+    public function beforeSave($options = array()) {
+        if (isset($this->data[$this->alias]['password'])) {
+            $passwordHasher = new BlowfishPasswordHasher();
+            $this->data[$this->alias]['password'] = $passwordHasher->hash(
+                $this->data[$this->alias]['password']
+            );
+        }
+        return true;
+    }
 }
